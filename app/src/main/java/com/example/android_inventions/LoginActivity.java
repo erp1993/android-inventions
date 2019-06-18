9863e75637e39c1921f9876e7f2f8446ada745e0
Eduardo <eduardoromeropalencia@gmail.com>
2019-06-18 13:59:37 +0200

    Commit Inicial

--- app/src/main/java/com/example/android_inventions/LoginActivity.java
+++ app/src/main/java/com/example/android_inventions/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.android_inventions;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    protected void onStart(){
+
     }
 }
