4b1e66611ccaf2969c4e914ee645dcdbbd1ececd
Eduardo <eduardoromeropalencia@gmail.com>
2019-06-19 08:31:56 +0200

    Login Firebase Funcionando

--- app/src/main/java/com/example/android_inventions/LoginActivity.java
+++ app/src/main/java/com/example/android_inventions/LoginActivity.java
@@ -1,20 +1,80 @@
 package com.example.android_inventions;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity implements
+        View.OnClickListener{
+
+    private FirebaseAuth mAuth;
+
+    private EditText EmailField;
+    private EditText PasswordField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        EmailField = findViewById(R.id.emailField);
+        PasswordField = findViewById(R.id.passField);
+
+        findViewById(R.id.loginButton).setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
     protected void onStart(){
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
 
+        if (currentUser != null){
+            updateUI(currentUser);
+        }
+    }
+
+    private void signIn(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Autenticacion fallida.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            startActivity(new Intent(this, MainActivity.class));
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.loginButton) {
+            signIn(EmailField.getText().toString(), PasswordField.getText().toString());
+        }
     }
 }
